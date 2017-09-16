1ceea22827c648f3f9907582c462ab46850c4d62
krajnarayanan <k.rajnarayanan@gmail.com>
2017-09-16 00:53:46 +0000

    initial commit

---
+++ book-share/src/components/book/book-list-page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import bookData from '../../book/book-data.json';
+export default class BookListPage {
+  
+  render() {
+    return(
+      <div> book list </div>  
+    )
+  }
+}
